4e54845587247a0160d7b9b32342064325245b33
Truong Dao <ton.truong.dao@outlook.com>
2021-05-04 16:54:50 -0400

    Persisting KanBan Update

--- src/components/App.js
+++ src/components/App.js
@@ -6,16 +6,72 @@ import data from '../data'
 
 const App = () => {
 // ##### React States
-const [todo, setTodo] = useState(data)
+  const [todo, setTodo] = useState(data)
 
-  const onDragEnd = (event) => {
-    console.log(event)
+  // ##### Helpers
+
+  const onDragEnd = (result) => {
+    // Result Object Propertoes
+    // const result = {
+    //  draggableId: 'task-1'
+    //  type:'TYPE'
+    //  reason: 'DROP',
+    //  source: {
+    //    droppableId: 'column-1'
+    //    index: 0
+    //  },
+    //  destination: {
+    //    droppableId: 'column-1'
+    //    index: 1
+    //  }
+    // console.log(result)
+
+    const { destination, source, draggableId } = result;
+
+    if(!destination) { // Nothing to do when no destination
+      return
+    }
+
+    if( // Item dropped to the same destination
+      destination.drappableId === source.droppableId && 
+      destination.index === source.index) {
+        return
+      }
+
+    const column = todo.columns[source.droppableId]
+    const newTaskIds = Array.from(column.taskIds) // Makes an array from a copy of the todo State
+
+    // Splice one Item from the source's index
+    newTaskIds.splice(source.index, 1)
+    // console.log(newTasksIds)
+    // Remove no item from destination, added draggable Id
+    newTaskIds.splice(destination.index, 0, draggableId)
+    // console.log(newTasksIds)
+
+    // Creating new column to avoid mutating OG State
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    }
+
+    console.log(newColumn)
+
+    // Updating todo State to avoid mutating OG State
+    const newTodo = {
+      ...todo,
+      columns: {
+        ...todo.columns,
+        [newColumn.id]: newColumn
+      }
+    }
+
+    setTodo(newTodo)
   }
 
   return (
     // Added DragDropContext to Enable Kanban
     <DragDropContext onDragEnd={onDragEnd}>
-      {data.columnOrder.map(columnId => {
+      {todo.columnOrder.map(columnId => {
         const column = todo.columns[columnId];
         const tasks = column.taskIds.map(taskId => todo.tasks[taskId]);
 
--- src/data/index.js
+++ src/data/index.js
@@ -1,18 +1,18 @@
 const data = {
   tasks: {
-    'firstTask': { id: 'task-1', content: 'Take Out The Garbage'},
-    'secondTask': { id: 'task-2', content: 'Watch My Favorite Show'},
-    'thirdTask': { id: 'task-3', content: 'Charge My Phone'},
-    'fourthTask': { id: 'task-4', content: 'Cook Dinner'}
+    'firstTask': { id: 'firstTask', content: 'Take Out The Garbage'},
+    'secondTask': { id: 'secondTask', content: 'Watch My Favorite Show'},
+    'thirdTask': { id: 'thirdTask', content: 'Charge My Phone'},
+    'fourthTask': { id: 'fourthTask', content: 'Cook Dinner'}
   },
   columns: {
     'firstCol' : { 
-      id: 'column-1',
+      id: 'firstCol',
       title: 'To Do',
       taskIds: ['firstTask', 'secondTask', 'thirdTask', 'fourthTask']
     },
     'secondCol' : { 
-      id: 'column-2',
+      id: 'secondCol',
       title: 'Completed',
       taskIds: []
     }
